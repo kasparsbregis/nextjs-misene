df2fb1b40a2cb13660482f659f330f6810dbf48d
Kaspars <kaspars.bregis@gmail.com>
2024-11-18 15:01:45 +0200

    TailwindCSS Setup and Theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,9 @@ const spaceGrotesk = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description: "A bit better stack overflow site",
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
     icon: "/images/site-logo.svg",
   },
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 const Home = () => {
   return (
     <>
-      <h1 className="text-3xl text-[#FF7000]">
-        Welcome to the world of Next.js
-      </h1>
-      <h1 className="font-space-grotesk text-3xl text-[#FF7000]">
-        Welcome to the world of Next.js
-      </h1>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
     </>
   );
 };
