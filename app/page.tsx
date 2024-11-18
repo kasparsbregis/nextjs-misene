58aaa518bf453ac8e2f52de80bdf39fe14d70b2e
Kaspars <kaspars.bregis@gmail.com>
2024-11-18 14:41:04 +0200

    Added local fonts and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+const Home = () => {
   return (
     <>
-      <h1 className="text-3xl text-white">Hello</h1>
+      <h1 className="text-3xl text-[#FF7000]">
+        Welcome to the world of Next.js
+      </h1>
+      <h1 className="font-space-grotesk text-3xl text-[#FF7000]">
+        Welcome to the world of Next.js
+      </h1>
     </>
   );
-}
+};
+
+export default Home;
